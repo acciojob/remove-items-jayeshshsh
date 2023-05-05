08429df247bdf05c29b09e7b999262a3feebb37e
Jayesh Sharma <jayeshsh1707@gmail.com>
2023-05-05 09:45:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const colorSelect=document.getElementById("colorSelect");
+const removeBtn=document.querySelector('input[type="button"]');
+
+removeBtn.addEventListener("click",function()
+{
+	const selectedOpt=colorSelect.options[colorSelect.selectedIndex];
+	colorSelect.removeChild(selectedOpt);
+});
